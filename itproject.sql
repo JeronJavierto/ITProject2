e5561fd721632171e7ae21acc5dfa7dfe36a8546
2164165 <37785711+2164165@users.noreply.github.com>
2019-02-27 21:44:53 +0800

    Add files via upload
    Hati kami ni destine

---
+++ itproject.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 27, 2019 at 02:42 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `itproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `Client_id` int(255) NOT NULL,
+  `ResID` int(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`Client_id`, `ResID`, `first_name`, `last_name`, `email`, `password`) VALUES
+(1, 1, 'Jeron', 'Javierto', 'Jeron@gmail.com', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment`
+--
+
+CREATE TABLE `equipment` (
+  `EquipID` int(255) NOT NULL,
+  `equipName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `equipment`
+--
+
+INSERT INTO `equipment` (`EquipID`, `equipName`) VALUES
+(1, 'chairs'),
+(2, 'microphone');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facility`
+--
+
+CREATE TABLE `facility` (
+  `FacID` int(255) NOT NULL,
+  `facilityName` varchar(255) NOT NULL,
+  `room` varchar(255) NOT NULL,
+  `roomType` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `capacity` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `facility`
+--
+
+INSERT INTO `facility` (`FacID`, `facilityName`, `room`, `roomType`, `description`, `capacity`) VALUES
+(1, 'Amphitheater', 'Amphitheater', 'Stage Area', 'Amphitheater', 1000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offices`
+--
+
+CREATE TABLE `offices` (
+  `office_id` int(255) NOT NULL,
+  `officeName` varchar(255) NOT NULL,
+  `person_in_charge` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `offices`
+--
+
+INSERT INTO `offices` (`office_id`, `officeName`, `person_in_charge`) VALUES
+(1, 'Student Affairs Office', 'Andrew Macalma');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `ResID` int(255) NOT NULL,
+  `resDate` date NOT NULL,
+  `startTime` time(6) NOT NULL,
+  `endTime` time(6) NOT NULL,
+  `resStatus` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`ResID`, `resDate`, `startTime`, `endTime`, `resStatus`) VALUES
+(1, '2018-11-07', '04:00:00.000000', '06:00:00.000000', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+
+CREATE TABLE `services` (
+  `SerID` int(255) NOT NULL,
+  `typeOfService` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `services`
+--
+
+INSERT INTO `services` (`SerID`, `typeOfService`) VALUES
+(1, 'Janitor'),
+(2, 'Security Guard');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
