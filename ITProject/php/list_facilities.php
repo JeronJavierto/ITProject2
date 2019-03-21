876d48181fc8bcdda6db0528649d51845a68119a
JeronJavierto <37394437+JeronJavierto@users.noreply.github.com>
2019-03-21 15:35:31 +0800

    shows the list of facilities (backend)

---
+++ ITProject/php/list_facilities.php
@@ -0,0 +1,43 @@
+<?php
+	include('DBConnector.php');	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>List of Facilities</title>
+</head>
+<body>
+	<table>
+		<tr>
+			<th>Level</th>
+			<th>Room</th>
+			<th>Room Type</th>
+			<th>Description</th>
+			<th>Capacity</th>
+			<th>Reservation</th>
+		</tr>
+<?php
+	$sql = "SELECT * FROM facility";
+	$result = $conn-> query($sql);
+
+	if ($result-> num_rows > 0){
+		while ($row = $result-> fetch_assoc()){
+			echo "<tr>
+					<td>" . $row["Level"] . "</td>
+					<td>" . $row["room"] . "</td>
+					<td>" . $row["roomType"] . "</td>
+					<td>"  . $row["description"] . "</td>
+					<td>" . $row["capacity"] . "</td>
+				  </tr>";
+		}
+		echo "</table>";
+	}else{
+		echo "0 result";
+	}
+
+	$conn-> close();
+?>		
+	</table>
+</body>
+</html>
